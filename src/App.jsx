d74234e9e800ca07b5c9e1c76aca697c114ac65e
Archontis Kostis <arxontisk02@gmail.com>
2024-06-01 17:06:44 +0300

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import QuizPage from "./pages/QuizPage/QuizPage.jsx";
 import WrongQuestionsPage from "./pages/WrongQuestionsPage/WrongQuestionsPage.jsx";
